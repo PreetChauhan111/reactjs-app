f8d4977762bf4567ff28dac6e48bd94fe8bf9170
Preet Chauhan <preetchauhan421@gmail.com>
2025-11-12 12:51:19 +0530

    changes on title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to React App!
+          Welcome to React App! Preet Chauhan
         </p>
         <a
           className="App-link"
